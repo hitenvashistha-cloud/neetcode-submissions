07ee94115041340e561eb1c2ed97bcafbc3e548c
Hiten Vashistha <hitenvashistha@gmail.com>
2026-08-01 19:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.09%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-      
-        for(int i = 0 ; i < haystack.size(); i++){
-            for(int j  = 0 ; j < needle.size();j++){
-                if(haystack[i+j] != needle[j]){
+
+        if (needle.size() > haystack.size())
+            return -1;
+
+        for (int i = 0; i <= haystack.size() - needle.size(); i++) {
+            for (int j = 0; j < needle.size(); j++) {
+                if (haystack[i + j] != needle[j])
                     break;
-                }
-                if(j == needle.size() - 1){
+
+                if (j == needle.size() - 1)
                     return i;
-                }
             }
         }
+
         return -1;
     }
 };

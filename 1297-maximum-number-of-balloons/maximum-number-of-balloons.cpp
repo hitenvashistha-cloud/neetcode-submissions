ded3f3d9295c38023e6f7752ac3d0f7243f95dfc
Hiten Vashistha <hitenvashistha@gmail.com>
2026-08-06 18:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (87.96%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        string s= "balloon";
         int hash[26] ={0};
-        int n  = 0;
         
         for(char ch : text){
             hash[ch-'a']++;
         }
-        while(true){
-            for(char ch : s){   
-              if(--hash[ch- 'a'] < 0){
-                return n;
-            }    
-        } 
-        n++;
-        }
-        return 0 ;
+        return min({
+            hash['b' - 'a'],
+            hash['a' - 'a'],
+            hash['l' - 'a']/2,
+            hash['o' - 'a']/2,
+            hash['n' - 'a']
+    });
+        
     }
 };

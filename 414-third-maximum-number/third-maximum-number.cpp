796963f7cc1b93857c1912a7c6a1a6238d8e7de1
Hiten Vashistha <hitenvashistha@gmail.com>
2026-08-07 16:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (73.79%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -9,18 +9,18 @@ public:
             if(nums[i] == max || nums[i] == second_max || nums[i] == third_max){
                 continue;
             }
-            if(nums[i] > max ){
-                third_max = second_max;
-                 second_max = max;
-                  max = nums[i];
-            }
-            else if( nums[i] < max && nums[i] > second_max && nums[i] > third_max ){
-            third_max = second_max;
-             second_max = nums[i];
-            }
-            else if( nums[i] < max && nums[i] < second_max && nums[i] > third_max ){
-                third_max = nums[i];
-            }
+            if (nums[i] > max) {
+             third_max = second_max;
+               second_max = max;
+                max = nums[i];
+}
+ else if (nums[i] > second_max) {
+    third_max = second_max;
+    second_max = nums[i];
+}
+else if (nums[i] > third_max) {
+    third_max = nums[i];
+}
 
         }
         if(third_max == LONG_MIN){
